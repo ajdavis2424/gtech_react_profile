4ac279a721d8272aab44642c2fec1a5672610c7d
ALexander Davis <ajdavis2424@gmail.com>
2021-12-21 20:36:48 -0500

    componets built, starting hompage

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Profile from './PortfolioContainer/Home/Profile';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* render Components here */}
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Profile() {
+    return (
+        <div className='profile-container'>
+        <div className='profile-parent'>
+            <div className='profile-details'>
+                <div className='colz'>
+                    {/* <a href='#'>
+                        <i className='fa fa-instagram'></i>
+                    </a> */}
+                    <a href='https://www.youtube.com/channel/UCKNL0ZMFc0ELUTxp5mxHkVg'>
+                        <i className='fa fa-youtube-square'></i>
+                    </a>
+                    <a href='https://twitter.com/AJDavis63561360'>
+                        <i className='fa fa-twitter'></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
+}
