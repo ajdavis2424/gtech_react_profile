ebcd18a9d776c787c43afdb4f836b79a58dd2653
ALexander Davis <ajdavis2424@gmail.com>
2021-12-23 13:29:44 -0500

    front page frame is done

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Typical from 'react-typical'
 
 export default function Profile() {
     return (
@@ -14,6 +15,49 @@ export default function Profile() {
                     </a>
                     <a href='https://twitter.com/AJDavis63561360'>
                         <i className='fa fa-twitter'></i>
+                    </a>
+                </div>
+                <div className='profile-detail-name'>
+                    <span className='primary-text'>
+                        {" "}
+                        Hello, I'M <span className='highlighted-text'>Alexander J. Davis</span>
+                    </span>
+                </div>
+                <div className='profile-details-role'>
+                    <span className='primary-text'>
+                        {" "}
+                        <h1>
+                        {" "}
+                        <Typical
+                            loop={Infinity}
+                        // array of items-- items to be displayed
+                        steps={[
+                           
+                            "Full-Stack Web Developer 🖥 ",
+                            1000,
+                            "M.B.A. 📜 ",
+                            1000,
+                            "MERN Stack Developer 🖱",
+                            1000,
+                            "SQL Developer 🔴",
+                            1000,
+                            "NoSQL Developer 📱",
+                            1000,
+                        ]}
+                        />
+                        </h1>
+                        <span className='profile-role-tagline'>
+                            Building applications with front and back-end skils.
+                        </span>
+                    </span>
+                </div>
+                <div className='profile-options'>
+                    <button className='btn primar-btn'> 
+                    {" "}
+                    Hire Me {" "}
+                    </button>
+                    <a href='resumehere' download='full stack resum.pdk'>
+                        <button className='btn highlighted-btn'>Get Resume</button> 
                     </a>
                 </div>
             </div>
