a75b27c70fe45d7e3994b24a7c471735f6cfb93b
ALexander Davis <ajdavis2424@gmail.com>
2021-12-28 13:25:51 -0500

    adding header

---
+++ client/src/PortfolioContainer/Home/Header/Header.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+import {TOTAL_SCREENS, GET_SCREEN_INDEX} from '../../../utilities/commonUtils'
+import ScrollService from '../../../utilities/ScrollService'
+import {faBars} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import './Header.css'
+
+export default function Header() {
+    const [selectedScreen, setSelectedScreen] = useState(0)
+    const[showHeaderOptions, setShowHeaderOptions] = useState(false)
+
+    const updateCurrentScreen = (currentScreen) => {
+        if(currnetScreen || !currentScreen.screenInView)
+        return;
+        let screenIndex = GET_SCREEN_INDEX(currentScreen.screenInView)
+        if(screenIndex <0)
+        return 
+    }
+    let currentScreenBroadCaster = ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen)
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- client/src/PortfolioContainer/PortfolioContainer.js
+++ client/src/PortfolioContainer/PortfolioContainer.js
@@ -3,23 +3,22 @@ import { TOTAL_SCREENS } from "../utilities/commonUtils";
 
 export default function PortfolioContainer() {
   const mapAllScreens = () => {
-    return(
-    TOTAL_SCREENS.map((screen) =>(
-      (screen.component) ? 
+    return TOTAL_SCREENS.map((screen) =>
+      screen.component ? ( 
         <screen.component
           screenName={screen.screen_name}
           key={screen.screen_name}
           id={screen.screen_name}
         />
-       : 
+      ) : ( 
         <div key={screen.screen_name}></div>
-      ))
-    )
-  }
+      )
+    );
+  };
 
-  return (
+  return
     <div className="portfolio-container">
       {mapAllScreens()}
-    </div>
-  );
+    </div>;
+  
 }
